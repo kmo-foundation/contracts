7019a01cf1b00f0858c9f1814d57cf08056f69f6
KMO Foundation <cto@kmo.foundation>
2021-11-30 13:35:55 +0400

    initial commit

---
+++ contracts/Token.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract WrappedKMO is ERC20, AccessControl {
+  bytes32 public constant BRIDGE_ROLE = keccak256("BRIDGE");
+
+  constructor() ERC20("wKMO", "wKMO") {
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+    _mint(_msgSender(), 100_000_000e18);
+  }
+
+  function mint(address to, uint256 amount) public onlyRole(BRIDGE_ROLE) {
+    _mint(to, amount);
+  }
+
+  function burn(uint256 amount) public onlyRole(BRIDGE_ROLE) {
+    _burn(_msgSender(), amount);
+  }
+}
---
+++ test/token.js
@@ -0,0 +1,53 @@
+const WrappedKMO = artifacts.require("WrappedKMO");
+
+const { constants, expectRevert, send, expectEvent, ether, BN } = require('@openzeppelin/test-helpers');
+const { expect, use } = require('chai');
+
+contract('WrappedKMO', ([owner, bridge, user]) => {
+  let token;
+
+  beforeEach('setup', async () => {
+    token = await WrappedKMO.new();
+  });
+
+  it('init check', async () => {
+    expect(await token.name()).equals('wKMO');
+    expect(await token.symbol()).equals('wKMO');
+    expect((await token.totalSupply()).toString()).equals(web3.utils.toWei('100000000'));
+    expect((await token.balanceOf(owner)).toString()).equals(web3.utils.toWei('100000000'));
+    expect((await token.hasRole(Buffer.from('\x01'), owner)));
+  });
+
+  it('mint check', async () => {
+    await expectRevert(token.mint(user, web3.utils.toWei('100'), {from: owner}), 'revert AccessControl');
+    await expectRevert(token.mint(user, web3.utils.toWei('100'), {from: user}), 'revert AccessControl');
+    await expectRevert(token.mint(user, web3.utils.toWei('100'), {from: bridge}), 'revert AccessControl');
+
+    await token.grantRole(await token.BRIDGE_ROLE(), bridge);
+
+    await expectRevert(token.mint(user, web3.utils.toWei('100'), {from: owner}), 'revert AccessControl');
+    await expectRevert(token.mint(user, web3.utils.toWei('100')), 'revert AccessControl');
+
+    token.mint(user, web3.utils.toWei('100'), {from: bridge});
+
+    expect((await token.totalSupply()).toString()).equals(web3.utils.toWei('100000100'));
+    expect((await token.balanceOf(user)).toString()).equals(web3.utils.toWei('100'));
+  });
+
+  it('burn check', async () => {
+    await expectRevert(token.burn(web3.utils.toWei('100'), {from: owner}), 'is missing role 0x08fb31c3e81624356c3314088aa971b73bcc82d22bc3e3b184b4593077ae3278');
+    await expectRevert(token.burn(web3.utils.toWei('100'), {from: user}), 'is missing role 0x08fb31c3e81624356c3314088aa971b73bcc82d22bc3e3b184b4593077ae3278');
+    await expectRevert(token.burn(web3.utils.toWei('100'), {from: bridge}), 'is missing role 0x08fb31c3e81624356c3314088aa971b73bcc82d22bc3e3b184b4593077ae3278');
+
+    await token.grantRole(await token.BRIDGE_ROLE(), bridge);
+
+    await expectRevert(token.mint(user, web3.utils.toWei('100'), {from: owner}), 'is missing role 0x08fb31c3e81624356c3314088aa971b73bcc82d22bc3e3b184b4593077ae3278');
+    await expectRevert(token.mint(user, web3.utils.toWei('100')), 'revert AccessControl');
+
+    await token.mint(bridge, web3.utils.toWei('100'), {from: bridge});
+    await token.burn(web3.utils.toWei('100'), {from: bridge});
+
+    expect((await token.totalSupply()).toString()).equals(web3.utils.toWei('100000000'));
+    expect((await token.balanceOf(bridge)).toString()).equals(web3.utils.toWei('0'));
+  });
+});
